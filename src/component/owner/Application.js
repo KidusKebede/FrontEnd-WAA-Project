50c04e498ba36c999eb9c16201c42f15c15cb0dd
Daniel Bogale <12695448+danielBogale@users.noreply.github.com>
2023-02-09 21:37:12 -0600

    Applition approve

--- src/component/owner/Application.js
+++ src/component/owner/Application.js
@@ -1,19 +1,37 @@
-import './Application'
+import { act } from "@testing-library/react";
+import "./Application";
 
-const Application = ({ activity }) => {
+const Application = ({ activity, approve, reject }) => {
   return (
     <div id="application">
       <ul>
         <li>
-          <img src="https://media.istockphoto.com/id/983950204/photo/modern-house-in-the-daylight.jpg?s=612x612&w=0&k=20&c=TjmRL9-7IPsT_lRvrzomTdXeNm4T_s7EOx1ppOF-7ZQ="
-            alt="appliedHouses" width="500px" height="500px"></img>
-          <div id='boxed'>
-            <h1>PropertyType: {activity.property.propertyType}</h1>
-            <h1>Price: {activity.property.price}</h1>
-            <h1>Applicant: {activity.users.name}</h1>
+          <img
+            src="https://media.istockphoto.com/id/983950204/photo/modern-house-in-the-daylight.jpg?s=612x612&w=0&k=20&c=TjmRL9-7IPsT_lRvrzomTdXeNm4T_s7EOx1ppOF-7ZQ="
+            alt="appliedHouses"
+            width="500px"
+            height="500px"
+          ></img>
+          <div id="boxed">
+            {/* <h1>Price: {activity.property.price}</h1> */}
+            {/* <h1>Applicant: {activity.users.name}</h1> */}
             <h1>Application Date: {activity.date}</h1>
-            <button>Approve</button>
-            <button>Reject</button>
+            <h1>Status: {activity.status}</h1>
+            {activity.status == "applied" ? (
+              <button
+                onClick={() =>
+                  approve(activity.id, {
+                    ...activity,
+                    status: "Approved",
+                  })
+                }
+              >
+                Approve
+              </button>
+            ) : (
+              <button>Revoke</button>
+            )}
+            <button onClick={() => reject(activity.id)}>Reject</button>
           </div>
         </li>
       </ul>
